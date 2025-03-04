05865673c29f8bca2aaaced5daa09ffc798d437c
damiancodes <damianbwire88@gmail.com>
2025-03-04 10:31:40 +0300

    commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,23 @@
-from django.urls import path
-from .import views
 
+from django.urls import path
+from . import views
+
+# urlpatterns = [
+#     path('', views.user_list),
+#     path('Add/', views.add_user),
+#     path('edit/<id>', views.EditUser),
+#     path('delete/<eid>', views.DeleteUser),
+#     path('View/<eid>', views.ViewUser),
+# ]
+
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-path('', views.user_list),
+    path('', views.user_list),
     path('Add/', views.add_user),
-
+    path('Edit/<id>', views.EditUser),
+    path('Delete/<eid>', views.DeleteUser),
+    path('View/<id>', views.ViewUser),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -19,5 +19,24 @@ def add_user(request):
     return render(request,'add.html',context={'form':form})
 
 
+def EditUser(request,id=None):
+    one_rec=User.objects.get(pk=id)
+    form=UserForm(request.POST or None,request.FILES or None, instance=one_rec)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    mydict= {'form':form}
+    return render(request,'edit.html',context=mydict)
 
+def DeleteUser(request,eid=None):
+    one_rec = User.objects.get(pk=eid)
+    if  request.method=="POST":
+         one_rec.delete()
+         return redirect('/')
+    return render(request,'delete.html')
 
+def ViewUser(request,id=None):
+    mydict={}
+    one_rec = User.objects.get(pk=id)
+    mydict['user']=one_rec
+    return render(request,'view.html',mydict)
